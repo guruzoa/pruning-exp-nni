a22c792c830951e4fb70b305cfb208c8c8338d07
suiguoxin <suiguoxin@gmail.com>
2020-07-23 09:12:16 +0000

    update

--- examples/submit_job.py
+++ examples/submit_job.py
@@ -9,7 +9,7 @@ with open('examples/token.txt', 'r') as file:
 
 headers = {'Authorization': 'Bearer ' + auth_token, 'Content-Type': 'text/plain'}
 
-def submit_job(prefix, model, pruner, sparsity, fine_tune_epochs, dataset='cifar10'):
+def submit_job(prefix, model, pruner, sparsity, pretrain_epochs, fine_tune_epochs, dataset='cifar10'):
     with open("examples/auto_pruners_torch_pai_ne_template.yml", 'r') as stream:
         job_config = yaml.safe_load(stream)
 
@@ -21,16 +21,17 @@ def submit_job(prefix, model, pruner, sparsity, fine_tune_epochs, dataset='cifar
     - python3 -m pip install tensorboard thop \
     - cd pruning-exp-nni && python3 examples/auto_pruners_torch.py \
       --model {model} \
-      --dataset {dataset}\
+      --dataset {dataset} \
       --load-pretrained-model True \
-      --pretrain-epochs 100 \
+      --pretrain-epochs {pretrain_epochs} \
       --pretrained-model-dir /mnt/confignfs-data/sgx/experiment_data/{dataset}/{model}/L1FilterPruner/01/model_trained.pth \
       --pruner {pruner} --base-algo l1 --cool-down-rate 0.9 \
       --sparsity {sparsity} \
       --speed-up True \
       --fine-tune True --fine-tune-epochs {fine_tune_epochs} \
       --data-dir /mnt/confignfs-data/sgx/{dataset} \
-      --experiment-data-dir /mnt/confignfs-data/sgx/experiment_data/{dataset}/{model}/{pruner}/{sparsity_str}".format(dataset=dataset, model=model, pruner=pruner, sparsity=sparsity, sparsity_str=str(sparsity).replace('.', ''), fine_tune_epochs=fine_tune_epochs)
+      --experiment-data-dir /mnt/confignfs-data/sgx/experiment_data/{dataset}/{model}/{pruner}/{sparsity_str}".format(
+          dataset=dataset, model=model, pruner=pruner, sparsity=sparsity, sparsity_str=str(sparsity).replace('.', ''), pretrain_epochs=pretrain_epochs, fine_tune_epochs=fine_tune_epochs)
     
     job_config["taskRoles"]["taskrole"]["commands"] = commands.split('- ')
     job_config["name"] = "{prefix}_{model}_{dataset}_{pruner}_{sparsity_str}".format(prefix=prefix, dataset=dataset, model=model, pruner=pruner, sparsity=sparsity, sparsity_str=str(sparsity).replace('.', ''))
@@ -41,13 +42,21 @@ def submit_job(prefix, model, pruner, sparsity, fine_tune_epochs, dataset='cifar
 
 
 if __name__ == '__main__':
-    models = ['vgg16']
+    # models = ['vgg16']
+    # pruners = ['ActivationMeanRankFilterPruner', 'ActivationAPoZRankFilterPruner']#'L1FilterPruner', 'SimulatedAnnealingPruner', 'NetAdaptPruner', 'AutoCompressPruner']
+    # sparsities = ['0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '0.95', '0.975']
+    # for model in models:
+    #     for pruner in pruners:
+    #         for sparsity in sparsities:
+    #             submit_job('0723', model, pruner, sparsity, pretrain_epochs=100, fine_tune_epochs=100)
+
+    models = ['resnet18']
     pruners = ['ActivationMeanRankFilterPruner', 'ActivationAPoZRankFilterPruner']#'L1FilterPruner', 'SimulatedAnnealingPruner', 'NetAdaptPruner', 'AutoCompressPruner']
     sparsities = ['0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '0.95', '0.975']
     for model in models:
         for pruner in pruners:
             for sparsity in sparsities:
-                submit_job('0723', model, pruner, sparsity, fine_tune_epochs=100)
+                submit_job('0723', model, pruner, sparsity, pretrain_epochs=200, fine_tune_epochs=200)
 
     # models = ['resnet50']
     # pruners = ['L1FilterPruner']
