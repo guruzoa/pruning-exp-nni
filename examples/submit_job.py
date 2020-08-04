1468b25f8a2ebb66601b4cc9e23d5d36545242d2
Ningxin <Ningxin.Zheng@microsoft.com>
2020-08-04 01:51:34 +0000

    update

--- examples/submit_job.py
+++ examples/submit_job.py
@@ -13,7 +13,7 @@ def submit_job(prefix, model, prune_type, sparsity, lr):
     with open("examples/auto_pruners_torch_pai_ne_template.yml", 'r') as stream:
         job_config = yaml.safe_load(stream)
 
-    job_config["taskRoles"]["taskrole"]["commands"] = commands.split('- ')
+    
     job_config["name"] = "{prefix}_{model}_{prune_type}_{sparsity}_{lr}".format(prefix=prefix, model=model, prune_type=prune_type, sparsity=str(sparsity).replace('.', ''), lr=str(lr).replace('.', ''))
 
     commands = "- ls /mnt/nniblob/yugzhan \
@@ -26,12 +26,12 @@ def submit_job(prefix, model, prune_type, sparsity, lr):
       --model {model} \
       --type {type} \
       --sparsity {sparsity} \
-      --finetune_epochs 20 \
+      --finetune_epochs 15 \
       --data-dir /mnt/all \
       --lr {lr} \
     - cp result.txt /mnt/confignfs-data/znx/{fname}".format(
         model=model, type=prune_type, sparsity=sparsity, lr=lr, fname=job_config["name"])
-    
+    job_config["taskRoles"]["taskrole"]["commands"] = commands.split('- ')
 
     print(yaml.dump(job_config))
     r = requests.post(url, headers=headers, data=yaml.dump(job_config))
@@ -39,38 +39,16 @@ def submit_job(prefix, model, prune_type, sparsity, lr):
 
 
 if __name__ == '__main__':
-    # models = ['vgg16']
-    # pruners = ['ActivationMeanRankFilterPruner', 'ActivationAPoZRankFilterPruner']#'L1FilterPruner', 'SimulatedAnnealingPruner', 'NetAdaptPruner', 'AutoCompressPruner']
-    # sparsities = ['0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '0.95', '0.975']
-    # for model in models:
-    #     for pruner in pruners:
-    #         for sparsity in sparsities:
-    #             submit_job('0723', model, pruner, sparsity, pretrain_epochs=100, fine_tune_epochs=100)
 
-    LRs = [0.1, 0.01, 0.001]
-    models = ['resnet34', 'mobilenet_v2']
+    LRs = [ 0.01, 0.001]
+    # models = ['resnet18', 'resnet34', 'mobilenet_v2']
+    models = ['wide_resnet50_2']
     pruners = ['l1']#'L1FilterPruner', 'SimulatedAnnealingPruner', 'NetAdaptPruner', 'AutoCompressPruner']
     sparsities = ['0.1','0.3','0.5','0.7']
     for model in models:
         for pruner in pruners:
             for sparsity in sparsities:
                 for lr in LRs:
-                    submit_job('nizhen_0729', model, pruner, sparsity, lr)
+                    submit_job('0803_', model, pruner, sparsity, lr)
                     time.sleep(1)
 
-    # models = ['resnet50']
-    # pruners = ['L1FilterPruner']
-    # sparsities = ['0.1', '0.975']
-    # for model in models:
-    #     for pruner in pruners:
-    #         for sparsity in sparsities:
-    #             submit_job('0723', model, pruner, sparsity, fine_tune_epochs=200)
-    
-    # # prepare pretrained models
-    # models = ['resnet18', 'vgg16']
-    # pruners = ['L1FilterPruner']
-    # sparsities = ['0.1']
-    # for model in models:
-    #     for pruner in pruners:
-    #         for sparsity in sparsities:
-    #             submit_job(model, pruner, sparsity)
