039ff457dca82f365dddb028a6d5552a6234d3cd
Ningxin <Ningxin.Zheng@microsoft.com>
2020-08-04 02:46:14 +0000

    benchmark

--- examples/submit_job.py
+++ examples/submit_job.py
@@ -9,46 +9,50 @@ with open('examples/token.txt', 'r') as file:
 
 headers = {'Authorization': 'Bearer ' + auth_token, 'Content-Type': 'text/plain'}
 
-def submit_job(prefix, model, prune_type, sparsity, lr):
+def submit_job(prefix, model, pruner, sparsity, pretrain_epochs, fine_tune_epochs, dataset='cifar10'):
     with open("examples/auto_pruners_torch_pai_ne_template.yml", 'r') as stream:
         job_config = yaml.safe_load(stream)
 
     
-    job_config["name"] = "{prefix}_{model}_{prune_type}_{sparsity}_{lr}".format(prefix=prefix, model=model, prune_type=prune_type, sparsity=str(sparsity).replace('.', ''), lr=str(lr).replace('.', ''))
-
-    commands = "- ls /mnt/nniblob/yugzhan \
-    - mkdir -p /mnt/imagenet \
-    - export AZCOPY_CRED_TYPE=\"Anonymous\"; azcopy copy \"https://nennistorage.blob.core.windows.net/nni/yugzhan/imagenet/all/?sv=2019-12-12&ss=b&srt=sco&sp=rwdlacx&se=2020-08-28T14:42:31Z&st=2020-07-27T06:42:31Z&spr=https&sig=EQCe7PQHP9lqflE6PVQpwJpQ9arDStkseBvqm9RakQU%3D\" \"/mnt\" --overwrite=prompt --check-md5 FailIfDifferent --from-to=BlobLocal --blob-type Detect --recursive; export AZCOPY_CRED_TYPE=\"\"; \
-    - cd /nni && git fetch && git reset --hard HEAD && git checkout constraint_pruner_tmp && git pull \
-    - cd examples/model_compress \
+    commands = "- apt update \
+    - apt install -y nfs-common \
+    - cd /nni/examples/model_compress \
     - python3 -m pip install tensorboard thop \
-    - python3 -u constrained_pruner.py \
+    - python3 auto_pruners_torch.py \
       --model {model} \
-      --type {type} \
+      --dataset {dataset} \
+      --load-pretrained-model True \
+      --pretrain-epochs {pretrain_epochs} \
+      --pretrained-model-dir /mnt/confignfs-data/znx/pretrianed_model/{dataset}/{model}/model_trained.pth \
+      --pruner {pruner} --base-algo l1 --cool-down-rate 0.9 \
       --sparsity {sparsity} \
-      --finetune_epochs 15 \
-      --data-dir /mnt/all \
-      --lr {lr} \
-    - cp result.txt /mnt/confignfs-data/znx/{fname}".format(
-        model=model, type=prune_type, sparsity=sparsity, lr=lr, fname=job_config["name"])
+      --speed-up True \
+      --fine-tune True --fine-tune-epochs {fine_tune_epochs} \
+      --data-dir /{dataset} \
+      --experiment-data-dir /mnt/confignfs-data/znx/experiment_data/{dataset}/{model}/{pruner}/{sparsity_str}".format(
+          dataset=dataset, model=model, pruner=pruner, sparsity=sparsity, sparsity_str=str(sparsity).replace('.', ''), pretrain_epochs=pretrain_epochs, fine_tune_epochs=fine_tune_epochs)
+    
     job_config["taskRoles"]["taskrole"]["commands"] = commands.split('- ')
+    job_config["name"] = "{prefix}_{model}_{dataset}_{pruner}_{sparsity_str}".format(prefix=prefix, dataset=dataset, model=model, pruner=pruner, sparsity=sparsity, sparsity_str=str(sparsity).replace('.', ''))
 
     print(yaml.dump(job_config))
     r = requests.post(url, headers=headers, data=yaml.dump(job_config))
     print(r.text)
 
-
 if __name__ == '__main__':
 
-    LRs = [ 0.01, 0.001]
-    # models = ['resnet18', 'resnet34', 'mobilenet_v2']
-    models = ['wide_resnet50_2']
-    pruners = ['l1']#'L1FilterPruner', 'SimulatedAnnealingPruner', 'NetAdaptPruner', 'AutoCompressPruner']
-    sparsities = ['0.1','0.3','0.5','0.7']
+    # models = ['resnet18']
+    # pruners = [ 'SimulatedAnnealingPruner']
+    # sparsities = ['0.1']
+    # for model in models:
+    #     for pruner in pruners:
+    #         for sparsity in sparsities:
+    #             submit_job('tdsmp', model, pruner, sparsity, pretrain_epochs=200, fine_tune_epochs=200)
+
+    models = ['resnet18', 'resnet50']
+    pruners = ['AutoCompressPruner', 'NetAdaptPruner', 'SimulatedAnnealingPruner']
+    sparsities = ['0.1', '0.3', '0.5', '0.7', '0.9', '0.95', '0.975']
     for model in models:
         for pruner in pruners:
             for sparsity in sparsities:
-                for lr in LRs:
-                    submit_job('0803_', model, pruner, sparsity, lr)
-                    time.sleep(1)
-
+                submit_job('0804', model, pruner, sparsity, pretrain_epochs=200, fine_tune_epochs=200)
